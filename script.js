cb1dec491a40a15e9aab7e7a3300b89f085810aa
Austine Gomez <austine.gomez@yahoo.com>
2023-05-17 23:20:36 -0500

    javascript

--- script.js
+++ script.js
@@ -1,50 +1,76 @@
-// Calculate the age based on the current date and user input
 function calculateAge() {
+    // Present date
     const currentDate = new Date();
-    const currentDay = currentDate.getDate();
-    const currentMonth = currentDate.getMonth() + 1;
-    const currentYear = currentDate.getFullYear();
-
-    const inputDay = Number(document.getElementById("day").value);
-    const inputMonth = Number(document.getElementById("month").value);
-    const inputYear = Number(document.getElementById("year").value);
-
-    // Validate input
+    const [currentDay, currentMonth, currentYear] = [
+      currentDate.getDate(),
+      currentDate.getMonth() + 1,
+      currentDate.getFullYear()
+    ];
+  
+    // Input date
+    const [inputDay, inputMonth, inputYear] = [
+      Number(document.getElementById('day').value),
+      Number(document.getElementById('month').value),
+      Number(document.getElementById('year').value)
+    ];
+  
+    // Months and days counts
     const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
- // Refactored code: function calculateAge() { const currentDate = new Date(), currentDay = currentDate.getDate(), currentMonth = currentDate.getMonth() + 1, currentYear = currentDate.getFullYear(), monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31], inputDay = Number(document.getElementById("day").value), inputMonth = Number(document.getElementById("month").value), inputYear = Number(document.getElementById("year").value);
-
-// Input validation
-if (inputMonth < 1 || inputMonth > 12 || inputDay < 1 || inputDay > monthDays[inputMonth - 1] || inputYear < 0) {
-    alert("Invalid input values entered! Please try again");
-    return;
-}
-
-// Calculate age in years, months and days
-let ageInDays = currentDay - inputDay,
-    ageInMonths = currentMonth - inputMonth,
-    ageInYears = currentYear - inputYear;
-
-if (ageInMonths < 0) {
-    ageInMonths += 12;
-    ageInYears--;
-}
-
-if (ageInDays < 0) {
-    ageInDays += monthDays[currentMonth - 1];
-    ageInMonths--;
-}
-
-// Update HTML with calculated age
-document.getElementById("ageInYears").innerHTML = ageInYears + " years";
-document.getElementById("ageInMonths").innerHTML = ageInMonths + " months";
-document.getElementById("ageInDays").innerHTML = ageInDays + " days";
-
-// Reset input fields
-document.getElementById("year").value = "";
-document.getElementById("month").value = "";
-document.getElementById("day").value = "";
-
-
-}
-
+  
+    // Alert messages
+    const alerts = {
+      month: document.getElementById('alert-month'),
+      day: document.getElementById('alert-day'),
+      year: document.getElementById('alert-year')
+    };
+  
+    // Input validation
+    if (inputMonth < 1 || inputMonth > 12) {
+      alerts.month.innerHTML = 'Must be a valid month';
+      return;
+    }
+    if (inputDay < 1 || inputDay > monthDays[inputMonth - 1]) {
+      alerts.day.innerHTML = 'Must be a valid day';
+      return;
+    }
+    if (inputYear < 1900 || inputYear > currentYear) {
+      alerts.year.innerHTML = 'Must be a valid year';
+      return;
+    }
+    if (
+      inputMonth === 2 &&
+      inputDay > 28 &&
+      (inputYear % 4 !== 0 || (inputYear % 100 === 0 && inputYear % 400 !== 0))
+    ) {
+      alerts.day.innerHTML = 'Must be a valid day';
+      return;
+    }
+  
+    // Calculate age in years, months, and days
+    let ageInDays = currentDay - inputDay;
+    let ageInMonths = currentMonth - inputMonth;
+    let ageInYears = currentYear - inputYear;
+  
+    if (ageInMonths < 0) {
+      ageInMonths += 12;
+      ageInYears--;
+    }
+  
+    if (ageInDays < 0) {
+      ageInDays += monthDays[currentMonth - 1];
+      ageInMonths--;
+    }
+  
+    // Update HTML with calculated age
+    document.getElementById('ageInYears').innerHTML = ageInYears + ' years';
+    document.getElementById('ageInMonths').innerHTML = ageInMonths + ' months';
+    document.getElementById('ageInDays').innerHTML = ageInDays + ' days';
+  
+    // Reset input fields
+    [day, month, year].forEach(() => {
+      document.getElementById().value = '';
+;
+    });
+  }
+  
 
