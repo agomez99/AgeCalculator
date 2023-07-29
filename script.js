2522a4f7d470b95bce5ce71f49847335723adf65
Austine Gomez <austine.gomez@yahoo.com>
2023-07-28 20:57:35 -0500

    Readme with links

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function calculateAge() {
+
   const currentDate = new Date();
   const currentDay = currentDate.getDate();
   const currentMonth = currentDate.getMonth() + 1;
@@ -8,9 +9,9 @@ function calculateAge() {
   const inputMonthElement = document.getElementById('month');
   const inputYearElement = document.getElementById('year');
 
-  const titleDay = document.getElementsByClassName('title-day');
-  const titleMonth = document.getElementsByClassName('title-month');
-  const titleYear = document.getElementsByClassName('title-year');
+  const errorTitleDay = document.getElementById('title-error');
+  const errorTitleMonth = document.getElementById('month-error');
+  const errorTitleYear = document.getElementById('year-error');
 
   const inputDay = Number(inputDayElement.value);
   const inputMonth = Number(inputMonthElement.value);
@@ -18,73 +19,81 @@ function calculateAge() {
 
   const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  const alerts = {
+  const errorMessages = {
     month: document.getElementById('alert-month'),
     day: document.getElementById('alert-day'),
     year: document.getElementById('alert-year'),
-     //titleMonth:document.getElementById('title-month'),
-    // titleDay:document.getElementById('title-day'),
-    // titleYear:document.getElementById('title-year')
   };
 
   const clearErrorMessage = (element) => {
     element.innerHTML = '';
   };
 
+  // Event listeners to clear error messages
   inputDayElement.addEventListener('input', () => {
-
-    clearErrorMessage(alerts.day);
-    //clearErrorMessage(alerts.titleDay);
+    clearErrorMessage(errorMessages.day);
+    errorTitleDay.style = '';
   });
 
   inputMonthElement.addEventListener('input', () => {
-    clearErrorMessage(alerts.month);
+    clearErrorMessage(errorMessages.month);
+    errorTitleMonth.style = '';
   });
 
   inputYearElement.addEventListener('input', () => {
-    clearErrorMessage(alerts.year);
+    clearErrorMessage(errorMessages.year);
+    errorTitleYear.style = '';
   });
 
-  for (const key in alerts) {
-    clearErrorMessage(alerts[key]);
+  // Clear error messages
+  for (const key in errorMessages) {
+    clearErrorMessage(errorMessages[key]);
   }
 
   let hasError = false;
 
+  // Validate input day
   if (inputDay === 0 || inputDay > monthDays[inputMonth - 1]) {
-    alerts.day.innerHTML = inputDay === 0 ? 'This field is required' : 'Must be a valid day';
-    //document.getElementById('title-day').classList.add('error-label');
-
+    errorMessages.day.innerHTML = inputDay === 0 ? 'This field is required' : 'Must be a valid day';
+    errorTitleDay.style = 'color:red';
     hasError = true;
-
   }
 
-
-
+  // Validate input month
   if (inputMonth < 1 || inputMonth > 12) {
-    alerts.month.innerHTML = inputMonth === 0 ? 'This field is required' :'Must be a valid month';
+    errorMessages.month.innerHTML = inputMonth === 0 ? 'This field is required' : 'Must be a valid month';
+    errorTitleMonth.style = 'color:red';
     hasError = true;
-
   }
 
+  // Validate input year
   if (inputYear < 1900 || inputYear > currentYear) {
-    alerts.year.innerHTML = inputYear === 0 ? 'This field is required' :'Must be a valid year';
+    errorMessages.year.innerHTML = inputYear === 0 ? 'This field is required' : 'Must be a valid year';
+    errorTitleYear.style = 'color:hsl(0, 100%, 67%)';
     hasError = true;
   }
 
-  if (inputMonth === 2 && inputDay > 31 && (inputYear % 4 !== 0 || (inputYear % 100 === 0 && inputYear % 400 !== 0))) {
-    alerts.day.innerHTML = 'Must be a valid day';
+  // Validate input day for February in leap years
+  if (inputMonth === 2 && inputDay > 29 && (inputYear % 4 !== 0 || (inputYear % 100 === 0 && inputYear % 400 !== 0))) {
+    errorMessages.day.innerHTML = 'Must be a valid day';
     hasError = true;
   }
 
+  // Return if there are any errors
   if (hasError) {
+        // Restore the input values in case of errors
+        inputDayElement.value = storedInputDay;
+        inputMonthElement.value = storedInputMonth;
+        inputYearElement.value = storedInputYear;
     return;
   }
 
+  // Calculate age
   let ageInDays = currentDay - inputDay;
   let ageInMonths = currentMonth - inputMonth;
   let ageInYears = currentYear - inputYear;
 
+  // Adjust months and years if necessary
   if (ageInMonths < 0) {
     ageInMonths += 12;
     ageInYears--;
@@ -95,21 +104,18 @@ function calculateAge() {
     ageInMonths--;
   }
 
+  // Display age
   document.getElementById('ageInYears').innerHTML = `<span class='color-purp'>${ageInYears}</span> years`;
   document.getElementById('ageInMonths').innerHTML = `<span class='color-purp'>${ageInMonths}</span> months`;
   document.getElementById('ageInDays').innerHTML = `<span class='color-purp'>${ageInDays}</span> days`;
 
-  inputDayElement.value = '';
-  inputMonthElement.value = '';
-  inputYearElement.value = '';
-  for (const key in alerts) {
-    clearErrorMessage(alerts[key]);
-  }
+  // Clear input fields
+  //inputDayElement.value = '';
+  //inputMonthElement.value = '';
+  //inputYearElement.value = '';
 
-
-
-  for (const key in alerts) {
-    alerts[key].innerHTML = '';
-    document.getElementById(key).classList.remove('error-label');
+  // Clear error messages
+  for (const key in errorMessages) {
+    clearErrorMessage(errorMessages[key]);
   }
 }
